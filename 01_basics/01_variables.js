f5d3fdce0207698a33e12bacd397a1d4ab5fdb3c
vyashiten <160244936+vyashiten@users.noreply.github.com>
2024-02-17 20:07:24 +0000

    01_variables changes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "hhhh@google.com"
+var accountPassword = "12345"
+accountCity = "Dayton"
+
+// accountId = 2 //not allowed
+
+accountEmail = "hv@hv.com"
+accountPassword = "2121211"
+accountCity = "Cincinnati"//possible to use memry space of variable without declaring variable
+let accountState;
+
+console.log(accountId);
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
