ffadb507793e8552169ade5ffad1e524b146234a
vyashiten <160244936+vyashiten@users.noreply.github.com>
2024-02-17 22:06:28 +0000

    comparison

---
+++ 01_basics/04_Comparison.js
@@ -0,0 +1,28 @@
+//console.log(2>1);
+//console.log(2>=1);
+//console.log(2<1);
+//console.log(2==1);
+//console.log(2!=1);
+
+//comparing two different datatypes
+//console.log("2" >1);//comparing two different datatypes
+//console.log("02" >1);
+
+/*
+
+//equality check  == and comparison > < >= <+ work differently 
+//comparison convert null to zero 
+console.log(null > 0);//copanrison convert null to zero, zero is not greater than zero. false
+console.log(null == 0);
+console.log(null >= 0);//JS converted null to zero, zero is greater than or equal to zero true
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined < 0);
+*/
+//=== strict check
+
+console.log("2"===2);
+console.log(2===2);
+
+
