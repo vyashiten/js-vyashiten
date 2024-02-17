bec0a162c39eb4a774a4a8a152b978fdf12b5d4b
vyashiten <160244936+vyashiten@users.noreply.github.com>
2024-02-17 20:30:31 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,40 @@
+"use strict";//treat all JS code as newer version
+
+//alert("hello")
+//alert( 3 + 3) // we are using node js, not browser
+
+console.log(3
+    +3); console.log("Hiten")//bad code redability
+
+//instead use this practice, code readability should be high
+console.log( 3 + 3);
+console.log("Hiten")
+
+
+let name = "hiten"
+let age = 18
+let isLoggedIn = false
+
+// common datatypes
+// number  = 1 to the power of 52 (or 53) 
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value, representation of empty value
+// undefined => value has not been assigned
+// symbol => symbols
+
+//object
+
+console.log(typeof "hiten")
+console.log(typeof age)
+console.log(typeof null)// null is an object in js
+console.log(typeof undefined)//output will be undefined, one type in JS
+
+
+
+
+
+
+
+
