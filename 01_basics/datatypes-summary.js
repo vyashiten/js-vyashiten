f6d788309f50bb75008c8a52c7f17be2510f7a38
vyashiten <160244936+vyashiten@users.noreply.github.com>
2024-02-18 20:43:23 +0000

    dataype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,63 @@
+/*
+primitive
+ 7 types:
+string
+
+number
+const score = 100
+const scorevalue = 100.3
+
+boolean
+const isLoggedIn = false
+
+null (empty)
+const outsideTemp = null
+
+undefined - variable declared in memory space declared but we don't know the value
+let userEmail;
+
+symbol - to make value unique (we wrap the button (or anything) in symbole to make it unique so that can be accessed)
+const Id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(Id===anotherId)
+
+BigInt -  usually we cover all in number datatype but some scientific or real big values comes under BigInt
+const bigNumber = 2345678909876543n put the n at the end and number will become bigint
+/*
+
+
+//Non primitive/reference type
+Arrays
+const heros = ["hero1","hero2","hero3"]
+
+Objects are in curley braces
+
+let myObj=
+{
+    name:"hiten",
+    age:11,
+}
+Functions
+
+function(){} -- function defination
+*/
+const myFunction = function(){
+    console.log("Hello world");
+}
+console.log(typeof bigNumber);
+/*
+Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object
+       */
